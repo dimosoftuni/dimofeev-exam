ef27aaa21df53095330550916b3f747f7b67c0e7
Dimo Mitev <dimosoftuni@gmail.com>
2023-12-07 20:19:28 +0200

    hotfix of the test

--- tests/ui/add-book.test.js
+++ tests/ui/add-book.test.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('Check add book page', async ({ page }) => {
     await page.goto('http://localhost:8080/add-book');
-    const form = await page.$('div');
+    const form = await page.$('form');
     expect(form).toBeTruthy();
   });
   
